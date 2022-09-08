66e63f39e560ac940fb43dedc3d0c551dfd6e063
thanatadcs <thanatad.cs@gmail.com>
2022-09-08 15:20:49 +0700

    Added libft, get_next_line, and ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 02:23:46 by tanukool          #+#    #+#             */
+/*   Updated: 2022/08/12 01:49:04 by tanukool         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(long n);
+int	ft_puthex(uintptr_t n, int mode);
+int	ft_printf(const char *s, ...);
+
+#endif
---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/06 21:50:58 by tanukool          #+#    #+#             */
+/*   Updated: 2022/09/08 14:47:02 by tanukool         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+ssize_t	get_char_index(char *s, char c);
+char	*get_next_line(int fd);
+
+#endif
